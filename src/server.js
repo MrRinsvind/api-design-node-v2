3ad5003cb1565a0f29aff5ed70986eb7d4e5c412
DRinsvind <adskiyprikol@gmail.com>
2018-07-27 11:47:18 +0300

    add midl

--- src/server.js
+++ src/server.js
@@ -1,16 +1,17 @@
 import express from 'express'
-
+import { restRouter } from './api'
+import { connect } from './db'
+import setupMiddware from './middleware'
+import { signin, protect } from './api/modules/auth'
 // Declare an app from express
 
 const app = express()
 
-const apiRouter = express.Router()
+setupMiddware(app)
+connect()
 
-apiRouter.get('/', (req,res)=>(
-  res.json({api: true, apirouter: false})
-))
-
-app.use('/api', apiRouter)
+app.use('/signin', signin)
+app.use('/api', protect, restRouter)
 
 app.all('*', (req,res)=>{
   res.json({ok:true})
